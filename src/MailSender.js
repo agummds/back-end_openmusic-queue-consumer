d75d6a126a158dac6e7bbba485fbc6a751f9c5b4
unknown <medisaagum@.gmail.com>
2024-09-14 21:01:34 +0700

    fix error kredensial SMTP_USER dan SMTP_PASSWORD

--- src/MailSender.js
+++ src/MailSender.js
@@ -7,8 +7,8 @@ class MailSender {
       port: process.env.SMTP_PORT,
       secure: true,
       auth: {
-        user: process.env.MAIL_ADDRESS,
-        pass: process.env.MAIL_PASSWORD,
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASSWORD,
       },
     });
   }
